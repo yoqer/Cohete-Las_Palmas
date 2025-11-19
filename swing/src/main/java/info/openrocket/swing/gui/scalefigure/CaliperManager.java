f94d4a3ebfa454db506b185bccd784d23e0a2f9b
SiboVG <sibo.vangool@hotmail.com>
2025-11-20 00:15:05 +0100

    Remove import

--- swing/src/main/java/info/openrocket/swing/gui/scalefigure/CaliperManager.java
+++ swing/src/main/java/info/openrocket/swing/gui/scalefigure/CaliperManager.java
@@ -35,7 +35,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 import java.util.EventObject;
 
